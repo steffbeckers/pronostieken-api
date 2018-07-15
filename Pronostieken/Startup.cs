927b857e956b78725aab6565953f420100bb3845
Steff Beckers <steff@steffbeckers.eu>
2018-07-15 23:11:12 +0200

    Adding Swagger from Swashbuckle (Install-Package Swashbuckle.AspNetCore) https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-2.1&tabs=visual-studio%2Cvisual-studio-xml

--- Pronostieken/Startup.cs
+++ Pronostieken/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Pronostieken.Data;
 using Pronostieken.Models;
 using Pronostieken.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Pronostieken
 {
@@ -37,6 +38,21 @@ namespace Pronostieken
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {
+                    Title = "Pronostieken API",
+                    Version = "v1",
+                    Contact = new Contact
+                    {
+                        Name = "Steff Beckers",
+                        Email = "steff@steffbeckers.eu",
+                        Url = "https://steffbeckers.eu"
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +72,16 @@ namespace Pronostieken
             app.UseStaticFiles();
 
             app.UseAuthentication();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pronostieken API v1");
+            });
 
             app.UseMvc(routes =>
             {
