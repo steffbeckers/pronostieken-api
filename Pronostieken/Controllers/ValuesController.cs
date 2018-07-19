c2718fa12578dc9265eae97c932fcca051636a3b
Steff Beckers <steff@steffbeckers.eu>
2018-07-19 21:41:38 +0200

    Swagger anotation updates to enrich the API

--- Pronostieken/Controllers/ValuesController.cs
+++ Pronostieken/Controllers/ValuesController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pronostieken.Models;
 
 namespace Pronostieken.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -23,15 +25,27 @@ namespace Pronostieken.Controllers
         /// Retrieves a specific value.
         /// </summary>
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Match> Get(int id)
         {
-            return "value";
+            return new Match() { Id = Guid.NewGuid(), Name = "Testing this", IsComplete = false };
         }
 
         /// <summary>
         /// Creates a specific value.
         /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     {
+        ///        "name": "Value1"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>            
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
         public void Post([FromBody] string value)
         {
         }
