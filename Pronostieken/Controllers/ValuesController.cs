a13d6584ca548512c468c87b91df380815a24a86
Steff Beckers <steff@steffbeckers.eu>
2018-07-19 21:27:12 +0200

    Configured the API to startup on root with Swagger. Added comments to the current endpoints.

--- Pronostieken/Controllers/ValuesController.cs
+++ Pronostieken/Controllers/ValuesController.cs
@@ -10,33 +10,43 @@ namespace Pronostieken.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        /// <summary>
+        /// Lists all values.
+        /// </summary>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Retrieves a specific value.
+        /// </summary>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Creates a specific value.
+        /// </summary>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        /// <summary>
+        /// Updates a specific value.
+        /// </summary>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        /// <summary>
+        /// Deletes a specific value.
+        /// </summary>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- Pronostieken/Startup.cs
+++ Pronostieken/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -40,6 +42,11 @@ namespace Pronostieken
                         Url = "https://steffbeckers.eu"
                     },
                 });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
